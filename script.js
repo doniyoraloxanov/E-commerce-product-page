5c014af67000e278ae9c35bc11435a9bc73f07f4
doniyoraloxanov <doniyoraloxanov@gmail.com>
2023-02-18 20:53:13 +0500

    Navigation of E-commerce page done

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const shoppingCart = document.querySelector(".shopping");
+const avatar = document.querySelector(".avatar");
+
+const openModel = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+shoppingCart.addEventListener("click", openModel);
+avatar.addEventListener("click", openModel);
+overlay.addEventListener("click", closeModal);
